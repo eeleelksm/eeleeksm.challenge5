28425815b56e028d871f3b37d61afddfd2645a5d
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-09-26 14:30:56 -0400

    color coding of time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,34 @@
-//Display today's date
+// Display today's date
 var dateToday = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(dateToday);
 
-// 
-$(".time-block").each(function() {
+var trackTime = function() {
+  let count = 9
   var currentHour = moment().hour();
-  var blockTime = ;
+  
+    // changing time block background color based on time
+  $(".time-block").each(function() {
+    var blockTime = count;
+    count++
+    // based on the time, change color for present, past and future
+    if (blockTime < currentHour) {
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    } 
+    else if (blockTime > currentHour) {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+    else {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    }
+  });
+}
 
-  if (blockTime < currentHour) {
-    $(this).removeClass("present");
-    $(this).removeClass("future");
-    $(this).addClass("past");
-  } 
-  else if (blockTime > currentHour) {
-    $(this).removeClass("present");
-    $(this).removeClass("past");
-    $(this).addClass("future");
-  }
-  else {
-    $(this).removeClass("past");
-    $(this).removeClass("future");
-    $(this).addClass("present");
-  }
-});
+trackTime();
 
 
