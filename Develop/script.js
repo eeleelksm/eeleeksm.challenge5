c0e573a3831708dd4ba9a8a325fc2efa8b3cae0f
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-09-26 14:09:28 -0400

    time block for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,27 @@
 //Display today's date
 var dateToday = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(dateToday);
+
+// 
+$(".time-block").each(function() {
+  var currentHour = moment().hour();
+  var blockTime = ;
+
+  if (blockTime < currentHour) {
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+    $(this).addClass("past");
+  } 
+  else if (blockTime > currentHour) {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+  }
+  else {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+  }
+});
+
+
